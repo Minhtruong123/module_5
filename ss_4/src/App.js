3a9f09797b1810153a13c00359fb250f1daee818
Minhtruong123 <duongminhtruong1234@gmail.com>
2023-05-04 09:19:46 +0700

    update

---
+++ ss_4/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hook from './component/hook';
+
+function App() {
+  return (
+    <div className="App">
+      <Hook/>
+    </div>
+  );
+}
+
+export default App;
